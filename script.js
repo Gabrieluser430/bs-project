df4f49e62fd86bb1132935f5dec979787a59da24
Gabriel Oliveira <gabrielbarbieri430@gmail.com>
2023-05-11 19:41:41 -0300

    Script alterado

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const day = document.querySelector(".day")
-const month = document.querySelector(".month")
 const date = new Date()
-const newDay = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate() 
-const newMonth = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
-day.textContent = newDay;
-month.textContent = newMonth;
+const formattedDate = date.toLocaleDateString("pt-BR")
+const day = formattedDate.slice(0, 2)
+const month = formattedDate.slice(3, 5)
+document.querySelector(".day").textContent = day
+document.querySelector(".month").textContent = month

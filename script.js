2794c2dee1c3e3b8a73682987bb3b74f85b3da32
Gabriel Oliveira <gabrielbarbieri430@gmail.com>
2023-05-11 19:49:54 -0300

    EOF adicionado em todos arquivos

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const day = formattedDate.slice(0, 2)
 const month = formattedDate.slice(3, 5)
 document.querySelector(".day").textContent = day
 document.querySelector(".month").textContent = month
+
